bb7b9b1ce570100d7a8e771963bcee0ebbd50f6d
unknown <dayalanpalanisamy783@gmail.com>
2026-09-15 16:38:39 +0530

    Updated index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Git Session 1!");
+
+let name = "Santhoshi";
+let course = "Full Stack Development";
+
+console.log("Name:", name);
+console.log("Course:", course);
+console.log("Git is successfully connected to GitHub!");
